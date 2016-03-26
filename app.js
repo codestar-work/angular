333a6864b498a8e3e87e6dbd6def06faf832b116
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-03-27 00:17:20 +0700

    update

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
-import { Component, Inject } from 'angular2/core';
+import { Component } from 'angular2/core';
 import { bootstrap } from 'angular2/platform/browser';
-import { NgFor, NgIf } from 'angular2/common';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS }
-from 'angular2/router';
-
-@Component({
-	template: `Home Page`
-})
-class HomePage{}
-
-@Component({
-	template: `Data Page`
-})
-class DataPage{}
 
 @Component({
 	selector: 'app',
-	providers: [ROUTER_PROVIDERS],
-	directives: [ROUTER_DIRECTIVES],
-	template: `
-		<div class='btn-group'>
-			<a [routerLink]="['Home']" class='btn btn-default'>Home</a>
-			<a [routerLink]="['Data']" class='btn btn-default'>Data</a>
-		</div>
-		<br/>
-		<router-outlet></router-outlet>
-	`
+	template: `<i>Hello Angular 2</i>
+		<select>
+			<option *ngFor='#d of data'>{{ d }}</option>
+		</select>
+		`
 })
-@RouteConfig([
-	{ path: '', name: 'Home', component: HomePage, useAsDefault: true }
-	{ path: '/data', name: 'Data', component: DataPage }
-])
 class App {
+	constructor() {
+		this.data = ['Apple', 'Banana', 'Coconut'];
+	}
+
 }
 
 bootstrap(App);
